a674bd9526ccf8f8e4beeb0784d70a2316231426
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-14 19:27:16 +0700

    [Add] new Modul1 projects

---
+++ Modul1/Modul1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Modul1
+{
+    public partial class Modul1Page : ContentPage
+    {
+        public Modul1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
