4c3340e417b264fce24e1f437cbe2e6654cea733
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-28 13:36:54 +0700

    [Add] ListView, shows a list of item into ListView widget

---
+++ Modul3/Modul3/BindingListString.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Modul3
+{
+    public partial class BindingListString : ContentPage
+    {
+        public BindingListString()
+        {
+            InitializeComponent();
+
+            // Populate data into ListView
+            List<string> items = new List<string> { "First", "Second", "Third" };
+            listView.ItemsSource = items;
+
+            // Onclick Listener for each item
+            listView.ItemTapped += async (sender, e) => 
+            {
+                await DisplayAlert("Tapped", e.Item.ToString() + " was selected",
+                                   "OK");
+                ((ListView)sender).SelectedItem = null;
+            };
+        }
+    }
+}
---
+++ Modul3/Modul3/Modul3Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Modul3
+{
+    public partial class Modul3Page : ContentPage
+    {
+        public Modul3Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
