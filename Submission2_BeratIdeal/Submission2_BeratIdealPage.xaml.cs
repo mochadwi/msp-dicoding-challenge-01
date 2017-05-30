f9efd19857b9a555062a9ac3523b43dc153b90e1
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-31 05:59:03 +0700

    [Add] 0th initial commit

---
+++ Submission2_BeratIdeal/Submission2_BeratIdealPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Submission2_BeratIdeal
+{
+    public partial class Submission2_BeratIdealPage : ContentPage
+    {
+        public Submission2_BeratIdealPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
