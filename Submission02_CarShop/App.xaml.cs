5ce00a23790ca923c481daeddb5236b0badad63a
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-31 23:40:28 +0700

    [Add] 0th Initial commit

---
+++ Submission02_CarShop/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Submission02_CarShop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Submission02_CarShopPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Submission02_CarShop/Submission02_CarShopPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Submission02_CarShop
+{
+    public partial class Submission02_CarShopPage : ContentPage
+    {
+        public Submission02_CarShopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
