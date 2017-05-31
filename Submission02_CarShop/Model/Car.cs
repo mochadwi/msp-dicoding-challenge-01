eb9754f781be3f20d6285598c5e900ca91665ab0
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-31 23:55:29 +0700

    [Add] Car model - Default constructor - Getter and Setter

---
+++ Submission02_CarShop/Model/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace Submission02_CarShop.Model
+{
+    public class Car
+    {
+        private string _name, _info, _imgSource;
+        private int _price;
+
+        public Car()
+        {
+        }
+
+		// Constructor
+        public Car(string name, string info, string imgSource, int price)
+        {
+            Name = name;
+            Info = info;
+            ImgSource = imgSource;
+            Price = price;
+        }
+
+		// Getter & Setter
+		public string Name
+        {
+            get
+            {
+                return _name;
+            }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+					throw new ArgumentNullException();
+
+				this._name = value;
+			}
+        }
+
+        public string Info
+        {
+            get
+            {
+                return _info;
+            }
+            set
+			{
+				if (string.IsNullOrEmpty(value))
+					throw new ArgumentNullException();
+
+				this._info = value;
+			}
+        }
+
+        public string ImgSource
+        {
+            get
+            {
+                return _imgSource;
+            }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+					throw new ArgumentNullException();
+
+				this._imgSource = value;
+			}
+        }
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                this._price = value;
+            }
+        }
+    }
+}
