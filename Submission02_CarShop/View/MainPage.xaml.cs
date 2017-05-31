4fe6f83db616183867e48c8a43e09b8a49c94beb
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-01 03:46:40 +0700

    [Add] View - MainPage as navigation

---
+++ Submission02_CarShop/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Submission02_CarShop.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
