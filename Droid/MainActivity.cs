cab229e133ca8a6957f794d963a7ef35270ec007
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-01 15:48:03 +0700

    [Add] 0th Initial Commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Submission03_Inventory.Droid
+{
+    [Activity(Label = "Submission03_Inventory.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ Submission03_Inventory/Submission03_InventoryPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Submission03_Inventory
+{
+    public partial class Submission03_InventoryPage : ContentPage
+    {
+        public Submission03_InventoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
